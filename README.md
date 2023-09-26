e39cb8f63fc0c153129e180e6577d662fe35035a
Dogukan Kivrak <dogukankvrak@hotmail.com>
2023-09-26 17:18:26 +0300

    Version 0.2

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # Version 0.1
 First model scheme added with using tensorflow.keras with GPU support. First training phase has been concluded with batch size 16 and epoch of 10.
+Also first image of the validation dataset is selected and it's label predicted by model then plotted. It was false.
 First Scores are: 
-Accuracy: 0.4375
-Precision: 0.45625000000000004
-Recall: 0.4375
-F1 Score: 0.4431818181818181
-It is actually not good for starting, but i'm aware that i have started with very simple structure.
+- Accuracy: 0.4375
+- Precision: 0.45625000000000004
+- Recall: 0.4375
+- F1 Score: 0.4431818181818181
+- It is actually not good for starting, but i'm aware that i have started with very simple structure.
+
+# Version 0.2
+Model structure revised from 3 convolutional layers to 4. Also dense layer size multiplied by 4. Sample image plotting mechanism switched to random 4 images.
+- Accuracy: 0.5625
+- Precision: 0.7479166666666668
+- Recall: 0.5625
+- F1 Score: 0.5568181818181818
+Bugs: 
+- There are no individual representative for 2 classes in the validation set. That's why confusion matrix seems unfinished.
+- More complex approach can be done for model.
--- train.py
+++ train.py
@@ -9,13 +9,12 @@ import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import tensorflow.compat.v1 as tf
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, accuracy_score
 from tensorflow.compat.v1.keras.backend import set_session
 from tensorflow.keras.preprocessing import image_dataset_from_directory
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, BatchNormalization
+from tensorflow.keras import Sequential, optimizers
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Dropout
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.layers.experimental import preprocessing
 
@@ -62,17 +61,23 @@ model = Sequential([
     data_augmentation,
     Conv2D(32, (3, 3), activation='relu', input_shape=(100, 100, 3)),
     BatchNormalization(),
-    MaxPooling2D((2, 2)),
     Conv2D(64, (3, 3), activation='relu'),
-    BatchNormalization(),
     MaxPooling2D((2, 2)),
+    Dropout(0.25),
+    Conv2D(128, (3, 3), activation='relu'),
+    BatchNormalization(),
+    Conv2D(128, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Dropout(0.25),
+    
     Flatten(),
-    Dense(64, activation='relu'),
+    Dense(256, activation='relu'),
     Dense(len(train_ds.class_names), activation='softmax')
 ])
 
 # Compile the model
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+optimizer = optimizers.Adam(learning_rate=1e-4)
+model.compile(optimizer= optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # Save the model at the checkpoint where validation accuracy is maximum
 checkpoint = ModelCheckpoint('best_model.h5', monitor='val_accuracy', mode='max', save_best_only=True, verbose=1)
@@ -88,21 +93,37 @@ val_images, val_labels = next(iter(val_ds))
 predictions = np.argmax(model.predict(val_images), axis=1)
 
 print(f"Accuracy: {accuracy_score(val_labels, predictions)}")
-print(f"Precision: {precision_score(val_labels, predictions, average='weighted')}")
+print(f"Precision: {precision_score(val_labels, predictions, average='weighted', zero_division=1)}")
 print(f"Recall: {recall_score(val_labels, predictions, average='weighted')}")
 print(f"F1 Score: {f1_score(val_labels, predictions, average='weighted')}")
 
-# Plot Confusion Matrix
-plt.figure(figsize=(10, 7))
+# Plot Confusion Matrix using matshow instead of sns.heatmap
+plt.figure(figsize=(10, 10))
 cm = confusion_matrix(val_labels, predictions)
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=train_ds.class_names, yticklabels=train_ds.class_names)
-plt.xlabel('Predicted labels')
-plt.ylabel('True labels')
+plt.matshow(cm, fignum=1, cmap='Blues')
+plt.colorbar()
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.title('Confusion Matrix')
+plt.xticks(np.arange(len(train_ds.class_names)), train_ds.class_names, rotation=90)
+plt.yticks(np.arange(len(train_ds.class_names)), train_ds.class_names)
+for i in range(len(cm)):
+    for j in range(len(cm[i])):
+        plt.text(j, i, cm[i][j], ha='center', va='center')
 plt.show()
 
-# Plot a sample image with its predicted and actual label
-plt.figure(figsize=(5, 5))
-plt.imshow(val_images[0].numpy().astype('uint8'))
-plt.title(f"Actual: {train_ds.class_names[val_labels[0]]}, Predicted: {train_ds.class_names[predictions[0]]}")
-plt.axis('off')
+# Extract 4 random images and labels from the validation dataset
+images, labels = [], []
+
+for img, label in val_ds.unbatch().shuffle(buffer_size=100).take(4):
+    images.append(img)
+    labels.append(label)
+
+# Visualize the images
+plt.figure(figsize=(10,10))
+for i in range(4):
+    plt.subplot(2,2,i+1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(f'Actual: {train_ds.class_names[labels[i]]}')
+    plt.axis('off')
 plt.show()
